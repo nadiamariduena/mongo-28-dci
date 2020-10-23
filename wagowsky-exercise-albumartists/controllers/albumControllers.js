ec009071218aeaf6483691b9cfe742c85ba03afa
melissa <nadia.mm.mariduena@gmail.com>
2020-10-23 23:12:22 +0200

    controller album connected with schema albumMOdel | error handling

---
+++ wagowsky-exercise-albumartists/controllers/albumControllers.js
@@ -0,0 +1,43 @@
+// IMPORTING the RESPECTIVE MODEL
+//
+//
+const Album = require("../models/albumModel");
+
+//  Defining and exporting a specific Controller Method
+//  Get All Albums(entries) data stuff artist, yearReleased...  from the albums Collection
+// Using Async-Await and Try-catch
+
+exports.getAlbums = async (req, res) => {
+  /*
+                    const albums = await Album.find({});  line: 25
+
+    //it will return all the data inside the "album" which is
+     the schema stuff, and then transform it into the variable "albums", that
+      will accepted only if everything was fine with the request.
+
+
+
+     */
+  // HERE YOU HAVE TO SET UP THE TRY AND CATCH for the data of the albums
+  // if the
+
+  try {
+    const albums = await Album.find({});
+    res.status(200);
+    res.send(albums);
+  } catch (error) {
+    res.status(400); //show an error if the info was negative
+    res.send(error.message);
+  }
+};
+
+/*
+exports.getAlbums;
+
+connected to the server.js
+app.use("/api/albums", albumRoutes);
+app.use("/api/artists", artistRoutes);
+
+
+
+*/
---
+++ wagowsky-exercise-albumartists/controllers/artistControllers.js
@@ -0,0 +1,8 @@
+// IMPORTING the RESPECTIVE MODEL
+//
+//
+const Artist = require("../models/artistModel");
+
+//  Defining and exporting a specific Controller Method
+//  Get All Albums(entries) data stuff artist, yearReleased...  from the albums Collection
+// Using Async-Await and Try-catch
